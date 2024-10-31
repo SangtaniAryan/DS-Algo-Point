580f864670c6db9b037c8986c7c95a548abe5f04
Aryan Sangtani <72159493+SangtaniAryan@users.noreply.github.com>
2024-10-31 23:08:07 +0530

    Update Bracket.java

--- Java/Bracket.java
+++ Java/Bracket.java
@@ -1,21 +1,21 @@
-import java.util.*;
+import java.util.*; //import all files from java 
 
-public class Bracket {
-    public static void main(String[] args)
+public class Bracket { //Bracket names class created in public access modifiers 
+    public static void main(String[] args) //String args used to pass command-line arguments to a Java program
     {    
-         Scanner sc=new Scanner(System.in);
-        int flag=0;
+         Scanner sc=new Scanner(System.in); //scanner used 
+        int flag=0; //variable flag with integer data type setted with 0 value
         System.out.print("Enter the expression :"); //User interaction lines
         String S=sc.next(); //Alphabetic Characters to Process and Store in a variable
         Stack<Character> brace=new Stack<Character>();
         for(int i=0;i<S.length();i++) //defined variable I as integer data type 
         {
-            if(S.charAt(i)=='{' || S.charAt(i)=='[' || S.charAt(i)=='(')
+            if(S.charAt(i)=='{' || S.charAt(i)=='[' || S.charAt(i)=='(') //if condition applied with string , character, and And operation 
             { 
-                brace.add(S.charAt(i));
+                brace.add(S.charAt(i)); //code applied for if condition is true
 
             } //if condition is used with multiple AND function to retrieve proper results
-            if(brace.isEmpty())
+            if(brace.isEmpty()) //if condition used again with isEmpty ()
             {
                flag=1;
            
@@ -27,14 +27,14 @@ public class Bracket {
                 {
                     brace.pop();
                 } //if bracket is open from one side and also from other this if condition is apply
-                else
+                else //else part of code applied 
                 {
-                   flag=1;
+                   flag=1; //if condition is false then this part is executed 
                
                     break;
                 } // or else unbalance result is generate
             }
-            if(S.charAt(i)==']' )
+            if(S.charAt(i)==']' ) //if else if 
             {
                 if(brace.peek()=='[')
                 {
